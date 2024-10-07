f4c7b0889c4714b10e40b01e831ef5807f591c2c
shaniPolsky <shanipolsky@gmail.com>
2024-10-07 23:39:30 +0300

    Add files via upload

---
+++ Event (1).cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    internal class Event
+    {
+        private int id;
+        private string title;
+        private DateTime start;
+        private List<Event> eventList;
+
+        public int Id {
+           get { return id; }
+           set { id = value; }
+        }
+
+      
+        public string Title
+        {
+            get { return title; }
+            set { title = value; }
+        }
+
+
+        public DateTime Start
+        {
+            get { return start; }
+            set { start = value; }
+        }
+
+        
+
+        public Event(int id, string title, DateTime start, List<Event> eventList)
+        {
+            Id = id;
+            Title = title;
+            Start = start;
+            eventList = eventList;
+        }
+       
+
+        // שליפת כל הארועים 
+        public List<Event> getAllEvents()
+        {
+            return eventList;
+        }
+
+        //הוספת ארוע חדש
+     public void AddEvent(Event e)
+        {
+            eventList.Add(e);
+        }
+
+        public void updateTitle(int id, string newTitle)
+        {
+            var a = eventList.Where(n => n.id == id);
+            foreach (var n in a)
+            {
+                n.Title = newTitle;
+            }
+        }
+
+        public void remove(int id)
+        {
+            var a = eventList.Where(n => n.id == id);
+            foreach (var n in a)
+            {
+                eventList.Remove(n);
+            }
+        }
+
+
+    }
+}
---
+++ Program (1).cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Event
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            List<Event> list = new List<Event>();
+
+            
+            Event firstEvent = new Event(1, "Meeting", DateTime.Now, list);
+            firstEvent.AddEvent(new Event(2, "Workshop", DateTime.Now.AddHours(2), list));
+            firstEvent.AddEvent(new Event(3, "shopping", DateTime.Now.AddHours(5), list));
+
+        }
+    }
+}
